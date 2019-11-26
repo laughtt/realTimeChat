c72ea530f697ebdbea5ac2bd61e10883497e5598
laughtt <jcarpioherrera@gmail.com>
2019-11-26 08:26:24 -0500

    fa

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/laughtt/realTimeChat
+
+go 1.13
+
+require github.com/gorilla/websocket v1.4.1
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var addr = flag.String("addr", ":8080", "http service address")
+
+func serverHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "not Found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method not allowed", http.StatusNotFound)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
+}
+func main() {
+
+	flag.Parse()
+	hub := newHub()
+	go hub.run()
+	http.HandleFunc("/", serverHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serverWs(hub, w, r)
+	})
+	err := http.ListenAndServer(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe :", err)
+	}
+}
